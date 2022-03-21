56acbf3ef8292c5b5df9c7b96348c0dc6c01eea9
lukemunsch <lpmunsch@gmail.com>
2022-03-21 22:26:57 +0000

    add stripe facility to out checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -14,5 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KfrxTCFZ5MUgwOyPW7N4O2Hi881agVemSpuiBBg5chjcX3ghQSxGCq7CpXjoy0cr96cCylYXHyC800UpA94FKTG00kE2WsDgt',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
