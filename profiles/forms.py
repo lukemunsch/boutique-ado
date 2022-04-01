e741cfb59e5659777af777b15615df1675dac9ab
lukemunsch <lpmunsch@gmail.com>
2022-04-01 14:43:53 +0000

    fix checkout to prepopulate info for deliveries

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,22 +3,22 @@ from .models import UserProfile
 
 
 class UserProfileForm(forms.ModelForm):
-    """Setup our form for the order"""
     class Meta:
-        """set up meta for our form"""
         model = UserProfile
         exclude = ('user',)
 
     def __init__(self, *args, **kwargs):
-        """add placeholders and classes, remove auto-generated 
-        labels and set autofocus on first field"""
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
         super().__init__(*args, **kwargs)
         placeholders = {
             'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
             'default_street_address1': 'Street Address 1',
             'default_street_address2': 'Street Address 2',
-            'default_town_or_city': 'Town or City',
-            'default_postcode': 'Postal Code',
             'default_county': 'County, State or Locality',
         }
 
